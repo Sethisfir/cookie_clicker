b0af08a17c7f65bf181b3cb6c0b04602b8b93a27
Sethisfir <micka_8@hotmail.fr>
2016-12-15 11:07:51 +0100

    intégration fonction click + compteur

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var ClickCounterViewModel = function() {
+    this.numberOfClicks = ko.observable(0);
+ 
+    this.registerClick = function() {
+        this.numberOfClicks(this.numberOfClicks() + 1);
+    };
+ 
+    this.resetClicks = function() {
+        this.numberOfClicks(0);
+    };
+
+};
+ 
+ko.applyBindings(new ClickCounterViewModel());

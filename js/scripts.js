4bbeb9c4d6a25b1a23f1479f928176a11e8de2e6
Sethisfir <micka_8@hotmail.fr>
2016-12-15 14:53:30 +0100

    ajout reset java.sc

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var score = 0;
-var click = 0
+var click = 0;
 var btn = document.getElementById("btn");
+var btnr = document.getElementById("btnr");
 btn.onclick = function () {
 
     if (click >= 1) {   
@@ -12,3 +13,7 @@ btn.onclick = function () {
 }
 
 
+btnr.onclick = function() {
+	score = 0;
+	document.getElementById("afficheur").innerHTML =0;
+}

bb3a44a2a7da1923e4e3d170aadb97fdf2c65970
Yves Charvis <charvisyves@ymail.com>
2016-12-15 12:56:10 +0100

    modif 12h

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
-var ClickCounterViewModel = function() {
-    this.numberOfClicks = ko.observable(0);
- 
-    this.registerClick = function() {
-        this.numberOfClicks(this.numberOfClicks() + 1);
-    };
- 
-    this.resetClicks = function() {
-        this.numberOfClicks(0);
-    };
 
-};
- 
-ko.applyBindings(new ClickCounterViewModel());
+
+var ClickCounterViewModel = function() {
+   this.numberOfClicks = ko.observable(0);
+
+   this.registerClick = function() {
+       this.numberOfClicks(this.numberOfClicks() + 1);
+   }
+   this.resetClicks = function() {
+       this.numberOfClicks(0);
+   }
+
+}
+
+ko.applyBindings(new ClickCounterViewModel())

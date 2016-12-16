91d164ae98c8a31ab1f939dfbbd0fe3516e28b23
Sethisfir <micka_8@hotmail.fr>
2016-12-16 13:33:16 +0100

    save cookies

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
+
+///////////////////////////////////////////////VARIABLE\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
 var score = 0;
 var click = 0;
 var hscore = 0;
+
 var btn = document.getElementById("btn");
 var btnr = document.getElementById("btnr");
+
+///////////////////////////////////////////FONCTION SCORE\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
 btn.onclick = function () {
 
     if (click >= 1) {   
@@ -13,9 +20,11 @@ btn.onclick = function () {
     document.getElementById("afficheur").innerHTML = score;
 }
 
+///////////////////////////////////////////FONCTION RESET\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 btnr.onclick = function() {
 	if (score >= hscore) {
+		cookieset(score);
 		document.getElementById("hscore").innerHTML = score;
 		hscore = score
 		score = 0 
@@ -25,3 +34,8 @@ btnr.onclick = function() {
 	document.getElementById("afficheur").innerHTML =0;
 	}
 }
+
+
+function cookieset(hscore){
+			document.cookie='hscore=' +hscore+"; expires= Sun, 28 Feb 2099 00:00:00 UTC";
+				}

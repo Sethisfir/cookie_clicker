eba1b758906406fabe45fd5e86e2cbbc0e81be55
Sethisfir <micka_8@hotmail.fr>
2016-12-16 09:57:55 +0100

    modification html + java Hscore

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var score = 0;
 var click = 0;
+var hscore = 0;
 var btn = document.getElementById("btn");
 var btnr = document.getElementById("btnr");
 btn.onclick = function () {
@@ -14,6 +15,13 @@ btn.onclick = function () {
 
 
 btnr.onclick = function() {
+	if (score >= hscore) {
+		document.getElementById("hscore").innerHTML = score;
+		hscore = score
+		score = 0 
+		document.getElementById("afficheur").innerHTML =0;
+	} else {
 	score = 0;
 	document.getElementById("afficheur").innerHTML =0;
+	}
 }

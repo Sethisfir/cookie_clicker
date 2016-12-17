3fc36c5a7f7b4764fd923e8449c6f623e9284fec
Darell <alex03000@live.com>
2016-12-18 00:57:42 +0100

    sauvegarde cookie ok +multiplicateur ok

--- js/scripts.js
+++ js/scripts.js
@@ -4,40 +4,63 @@
 var score = 0;
 var click = 0;
 var hscore = 0;
-
+var multi = 2; 
+var prixmulti = 80;
+var prixmult = document.getElementById("prixmult")          //
+var mult = document.getElementById("mult");
 var btn = document.getElementById("btn");
 var btnr = document.getElementById("btnr");
+var afficheur =  document.getElementById("afficheur");
 
 ///////////////////////////////////////////FONCTION SCORE\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-btn.onclick = function () {
+prixmult.innerHTML ="prix: " + prixmulti;
 
-    if (click >= 1) {   
-        score = score + click;
-    } else {
-        score = score + 1;
-    }
-    document.getElementById("afficheur").innerHTML = score;
+btn.addEventListener("click", function () {
+	if (multi >= 3){
+		score += multi -1;
+	}
+	else {
+		score++;
+	}
+   
+    afficheur.innerHTML = score;
+}, false);
+
+function multiplicateur() {
+	mult.innerHTML = "X" + multi;
+	if (score < prixmulti) {
+		alert("Pas assez de cookies clic encore comme un abrutis parceque tu aime ça!!!!")
+	}
+	else if (score >= prixmulti) {
+		multi ++;
+		mult.innerHTML = "X" + multi;
+		score -= prixmulti;
+		prixmulti = prixmulti*2;
+		prixmult.innerHTML ="prix: " + prixmulti;
+    	afficheur.innerHTML = score;
+	}
+
+
+
 }
-
 ///////////////////////////////////////////FONCTION RESET\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 btnr.onclick = function() {
 	if (score >= hscore) {
 		cookieset(score);
-		document.getElementById("hscore").innerHTML = score;
-		hscore = score
 		score = 0 
-		document.getElementById("afficheur").innerHTML =0;
-	} else {
-	score = 0;
-	document.getElementById("afficheur").innerHTML =0;
+		afficheur.innerHTML = 0;
+	} 
+	else {
+		score = 0;
+		afficheur.innerHTML =0;
 	}
 }
 
 
 function cookieset(hscore){
-	document.cookie='hscore=' +hscore+"; expires= Sun, 28 Feb 2099 00:00:00 UTC; path=/"
+	document.cookie='hscore=' +hscore+"; expires= Sun, 14 may 2017 00:00:00 UTC; path=/"
 				}
 
 function readCookie(hscore) {
@@ -51,7 +74,15 @@ function readCookie(hscore) {
 	return null;	
 }
 
-var x = readCookie("hscore");
-if (x) {
-	document.getElementById("hscore").innerHTML =hscore;
-}
+	document.getElementById("hscore").innerHTML = readCookie("hscore");
+ 
+
+ window.onbeforeunload = function() {								//unbeforeunload "evenent provoqué par la fermeture de la fenêtre"
+ 	if (score > parseInt(readCookie("hscore"))){					//parseInt retranscrit une chaine de charecter en nombre ("12" = 12)
+ 		cookieset(score);
+ 	}
+ 	else if(!readCookie("hscore")){									//else if (sinon si) si le cookie hscre doesn't exist
+ 		cookieset = 0;												//we created it!
+ 	}
+ 	// body...
+ } 
